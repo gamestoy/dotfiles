9eb36e5c9df8fbf5c5b5716d95b0d20f13caf2b1
gamestoy <gaston.amestoy@teampicnic.com>
2024-03-17 19:56:16 +0100

    +Update

--- config/zsh/.zsh/alias.zsh
+++ config/zsh/.zsh/alias.zsh
@@ -1,2 +1,3 @@
-alias ls='exa -abhl --icons --color=always -s=type'
+alias ls='eza -abhl --icons --color=always -s=type'
 alias git-clean-local='git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch -d'
+alias search="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}' | xargs nvim"
